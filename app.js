76ef310a515f6e528da2aa060115d2e5dda9aeb8
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-04-17 16:12:25 -0700

    changed api to accept POST and added body parser

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/api/call/:callerName', function (req, res) {
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+
+app.post('/api/call/:callerName', function (req, res) {
+  console.log(req.body)
+  console.log(req.body.callDelay)
+  console.log(req.body.callRepeat)
+
   res.status(200);
   res.send('Hello World!');
 });
